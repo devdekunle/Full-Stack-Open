296dda59435cd519be8feb30eb15c66b95bb85a2
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2024-02-21 16:35:37 +0100

    phonebook step 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,15 +10,18 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({name:newName}))
-    setNewName('')
+    const checkName = persons.find(person => person.name === newName)
+    checkName ?
+      alert(`${newName} is already added to the phonebook`) :
+      setPersons(persons.concat({name:newName}))
+      setNewName('')       
   }
   return (
       <div>
         <h2>Phonebook</h2>
         <form onSubmit={handleSubmit}>
           <div>
-            name: <input value = {newName} onChange={(e)=>setNewName(e.target.value)}/>
+            name: <input placeholder={newName} value={newName} onChange={(e)=>setNewName(e.target.value)}/>
           </div>
           <div>
             <button type='submit'>add</button>
@@ -28,7 +31,6 @@ const App = () => {
         <div>
           {persons.map(person => <p key={person.name}>{person.name}</p>)}
         </div>
-        <div>debug: {newName}</div>
       </div>
   )
 }
